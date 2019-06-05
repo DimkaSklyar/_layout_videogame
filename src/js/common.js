1fd50673152ce8f2749acc2325f035f825e0c81c
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2019-06-06 00:09:58 +0300

    v0.1

---
+++ src/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  $('.my-slider').slick({
+    useTransform: true,
+    cssEase: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+    fade: true,
+    arrows: true
+  });
+});
